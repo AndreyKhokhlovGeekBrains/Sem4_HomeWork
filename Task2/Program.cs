fe54553b69fd75cf424bae99ebd08020f706a659
Andrey Khokhlov <kron-k@ya.ru>
2023-07-03 23:36:46 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int countNums(int i)
+{
+    int count = 0;
+    while (i > 0)
+    {
+        i /= 10;
+        count++;
+    }
+    return count;
+}
+
+int[] createArray(int number, int size)
+{
+    int index = 0;
+    int[] array = new int[size];
+
+    while (number > 0)
+    {
+        array[index] = number % 10;
+        number /= 10;
+        index++;
+    }
+    return array;
+}
+
+int sumNumbers(int[] array)
+{   
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine($"Введенное число состоит из {countNums(N)} цифр");
+Console.WriteLine($"Массив: [{string.Join(";", createArray(N, countNums(N)))}]");
+Console.WriteLine($"Сумма цифр в веденном числе {N} = {sumNumbers(createArray(N, countNums(N)))}");
