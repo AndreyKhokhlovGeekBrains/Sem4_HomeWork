44ed456a00a4af96c906c9d763e0087a27b20b50
Andrey Khokhlov <kron-k@ya.ru>
2023-07-04 20:45:22 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,31 @@
 // 6, 1, 33 -> [6, 1, 33]
 // (каждый элемент вводит человек)
 
+int size = 8;
+int[] myArray = new int[size];
+
+int[] createArray(int[] array)
+{
+    int i = 0;
+    while(i < array.Length)
+    {
+        Console.WriteLine("Введите число: ");
+        if(int.TryParse(Console.ReadLine(), out int num))
+        {
+            array[i] = num;
+            i++;
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное число");
+        }
+    }
+    return myArray;
+}
+
+void printArray(int[] array)
+{
+    Console.WriteLine($"Массив: [{string.Join(";", array)}]");
+}
+
+printArray(createArray(myArray));
